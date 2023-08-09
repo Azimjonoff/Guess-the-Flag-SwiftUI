a77ee096fb12c604a68e6190fe66bacfabdb6d6c
Sarvarbek Azimjonov <sarvarazimjonov7@gmail.com>
2023-08-09 12:57:59 +0500

    first commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Azimjonoff on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
